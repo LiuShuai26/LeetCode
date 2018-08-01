b02ee7408cd138f7bbfe934a2c231a88164a261c
Shuai <liustark@hotmail.com>
2018-08-01 01:24:00 -0400

    add 35

---
+++ Python/35.py
@@ -0,0 +1,22 @@
+
+
+def searchInsert(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    low = 0
+    high = len(nums)-1
+    while low <= high:
+        mid = int((low+high)/2)
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            high = mid-1
+        else:
+            low = mid+1
+    return low
+
+
+print(searchInsert([1, 3, 4, 6, 7, 9], 5))
