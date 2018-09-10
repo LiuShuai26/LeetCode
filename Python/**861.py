f5a03d4e1a6ca53e1c20148d6bf951aa7e9f1330
Shuai <liustark@hotmail.com>
2018-09-10 10:28:06 +0800

    10.10.2018

---
+++ Python/**861.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def matrixScore(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: int
+        """
+        R, C = len(A), len(A[0])
+        ans = 0
+        for c in range(C):
+            col = 0
+            for r in range(R):
+                col += A[r][c] ^ A[r][0]
+            ans += max(col, R-col) * 2 ** (C-1-c)
+        return ans
--- Python/814.py
+++ Python/814.py
@@ -11,4 +11,18 @@ class Solution:
         :type root: TreeNode
         :rtype: TreeNode
         """
-        
+        if not root:
+            return None
+        if root.val == 0:
+            if root.left:
+                root.left = self.pruneTree(root.left)
+            if root.right:
+                root.right = self.pruneTree(root.right)
+            if not root.left and not root.right:
+                return None
+        else:
+            if root.left:
+                root.left = self.pruneTree(root.left)
+            if root.right:
+                root.right = self.pruneTree(root.right)
+        return root
